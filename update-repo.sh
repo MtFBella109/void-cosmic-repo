48ea13b0de576c7a2239d270afdc2ca848fe23c0
MtFBella109 <belladev109@proton.me>
2025-07-08 04:13:34 +0200

    Fixed INSTALL

--- update-repo.sh
+++ update-repo.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-cp -n ~/void-packages/hostdir/binpkgs/cosmic*x86_64.xbps ~/void-cosmic-repo/repo/x86_64/
-cp -n ~/void-packages/hostdir/binpkgs/pop-launcher*x86_64.xbps ~/void-cosmic-repo/repo/x86_64/
-xbps-rindex -a ~/void-cosmic-repo/repo/x86_64/*.xbps
-xbps-rindex -S ~/void-cosmic-repo/repo/x86_64/*.xbps --privkey ~/repo-private-key.pem --signedby "Bella Wagner <belladev109@proton.me>"
-xbps-rindex -r ~/void-cosmic-repo/repo/x86_64/
+cp -n ~/void-packages/hostdir/binpkgs/cosmic*x86_64.xbps ~/cosmic-repo/repo/x86_64/
+cp -n ~/void-packages/hostdir/binpkgs/pop-launcher*x86_64.xbps ~/cosmic-repo/repo/x86_64/
+xbps-rindex -a ~/cosmic-repo/repo/x86_64/*.xbps
+xbps-rindex -S ~/cosmic-repo/repo/x86_64/*.xbps --privkey ~/repo-private-key.pem --signedby "Bella Wagner <belladev109@proton.me>"
+xbps-rindex -r ~/cosmic-repo/repo/x86_64/
 
 if [ "$1" != "glibc" ]; then
-	cp -n ~/void-packages/hostdir/binpkgs/cosmic*musl.xbps ~/void-cosmic-repo/repo/x86_64-musl/
-	cp -n ~/void-packages/hostdir/binpkgs/pop-launcher*musl.xbps ~/void-cosmic-repo/repo/x86_64-musl/
-	XBPS_TARGET_ARCH=x86_64-musl xbps-rindex -a ~/void-cosmic-repo/repo/x86_64-musl/*.xbps
-	xbps-rindex -S ~/void-cosmic-repo/repo/x86_64-musl/*.xbps --privkey ~/repo-private-key.pem --signedby "Bella Wagner <belladev109@proton.me>"
-	XBPS_TARGET_ARCH=x86_64-musl xbps-rindex -r ~/void-cosmic-repo/repo/x86_64-musl/
+	cp -n ~/void-packages/hostdir/binpkgs/cosmic*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
+	cp -n ~/void-packages/hostdir/binpkgs/pop-launcher*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
+	XBPS_TARGET_ARCH=x86_64-musl xbps-rindex -a ~/cosmic-repo/repo/x86_64-musl/*.xbps
+	xbps-rindex -S ~/cosmic-repo/repo/x86_64-musl/*.xbps --privkey ~/repo-private-key.pem --signedby "Bella Wagner <belladev109@proton.me>"
+	XBPS_TARGET_ARCH=x86_64-musl xbps-rindex -r ~/cosmic-repo/repo/x86_64-musl/
 fi
 git add .
 if [ -v 1 ] &&  [ "$1" != "glibc" ]; then
