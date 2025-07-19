c971c4c2d4aeff8fd650b6aef0dc8d6febaa1e3f
MtFBella109 <belladev109@proton.me>
2025-07-19 20:18:14 +0200

    Added catppuccin-sddm theme

--- update-repo.sh
+++ update-repo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp -n ~/void-packages/hostdir/binpkgs/cosmic*x86_64.xbps ~/cosmic-repo/repo/x86_64/
 cp -n ~/void-packages/hostdir/binpkgs/pop-launcher*x86_64.xbps ~/cosmic-repo/repo/x86_64/
+cp -n ~/void-packages/hostdir/binpkgs/catppuccin-sddm*x86_64.xbps ~/cosmic-repo/x86_64/
 xbps-rindex -a ~/cosmic-repo/repo/x86_64/*.xbps
 xbps-rindex -S ~/cosmic-repo/repo/x86_64/*.xbps --privkey ~/repo-private-key.pem --signedby "Bella Wagner <belladev109@proton.me>"
 xbps-rindex -r ~/cosmic-repo/repo/x86_64/
@@ -8,6 +9,7 @@ xbps-rindex -r ~/cosmic-repo/repo/x86_64/
 if [ "$1" != "glibc" ]; then
 	cp -n ~/void-packages/hostdir/binpkgs/cosmic*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
 	cp -n ~/void-packages/hostdir/binpkgs/pop-launcher*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
+	cp -n ~/void-packages/hostdir/binpkgs/catppuccin-sddm*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
 	XBPS_TARGET_ARCH=x86_64-musl xbps-rindex -a ~/cosmic-repo/repo/x86_64-musl/*.xbps
 	xbps-rindex -S ~/cosmic-repo/repo/x86_64-musl/*.xbps --privkey ~/repo-private-key.pem --signedby "Bella Wagner <belladev109@proton.me>"
 	XBPS_TARGET_ARCH=x86_64-musl xbps-rindex -r ~/cosmic-repo/repo/x86_64-musl/
