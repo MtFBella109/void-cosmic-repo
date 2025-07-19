b6624270df02ead1e27b7ade9b1ca337dde90b31
MtFBella109 <belladev109@proton.me>
2025-07-19 20:30:32 +0200

    Added the package cosmic-iso-flatpaks for the planed Void custom iso with cosmic pre-installed

--- update-repo.sh
+++ update-repo.sh
@@ -2,6 +2,7 @@
 cp -n ~/void-packages/hostdir/binpkgs/cosmic*x86_64.xbps ~/cosmic-repo/repo/x86_64/
 cp -n ~/void-packages/hostdir/binpkgs/pop-launcher*x86_64.xbps ~/cosmic-repo/repo/x86_64/
 cp -n ~/void-packages/hostdir/binpkgs/catppuccin-sddm*x86_64.xbps ~/cosmic-repo/x86_64/
+cp -n ~/void-packages/hostdir/binpkgs/cosmic-iso-flatpaks*x86_64.xbps ~/cosmic-repo/x86_64/
 xbps-rindex -a ~/cosmic-repo/repo/x86_64/*.xbps
 xbps-rindex -S ~/cosmic-repo/repo/x86_64/*.xbps --privkey ~/repo-private-key.pem --signedby "Bella Wagner <belladev109@proton.me>"
 xbps-rindex -r ~/cosmic-repo/repo/x86_64/
@@ -10,6 +11,7 @@ if [ "$1" != "glibc" ]; then
 	cp -n ~/void-packages/hostdir/binpkgs/cosmic*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
 	cp -n ~/void-packages/hostdir/binpkgs/pop-launcher*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
 	cp -n ~/void-packages/hostdir/binpkgs/catppuccin-sddm*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
+	cp -n ~/void-packages/hostdir/binpkgs/cosmic-iso-flatpaks*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
 	XBPS_TARGET_ARCH=x86_64-musl xbps-rindex -a ~/cosmic-repo/repo/x86_64-musl/*.xbps
 	xbps-rindex -S ~/cosmic-repo/repo/x86_64-musl/*.xbps --privkey ~/repo-private-key.pem --signedby "Bella Wagner <belladev109@proton.me>"
 	XBPS_TARGET_ARCH=x86_64-musl xbps-rindex -r ~/cosmic-repo/repo/x86_64-musl/
