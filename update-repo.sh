4df3631b4b9e9c39a999a454be2fe86775ff7516
MtFBella109 <belladev109@proton.me>
2025-08-30 13:54:54 +0200

    updated catppuccin-sddm

--- update-repo.sh
+++ update-repo.sh
@@ -2,7 +2,6 @@
 cp -n ~/void-packages/hostdir/binpkgs/cosmic*x86_64.xbps ~/cosmic-repo/repo/x86_64/
 cp -n ~/void-packages/hostdir/binpkgs/pop-launcher*x86_64.xbps ~/cosmic-repo/repo/x86_64/
 cp -n ~/void-packages/hostdir/binpkgs/catppuccin-sddm*x86_64.xbps ~/cosmic-repo/repo/x86_64/
-cp -n ~/void-packages/hostdir/binpkgs/cosmic-iso-flatpaks*x86_64.xbps ~/cosmic-repo/x86_64/
 xbps-rindex -a ~/cosmic-repo/repo/x86_64/*.xbps
 xbps-rindex -S ~/cosmic-repo/repo/x86_64/*.xbps --privkey ~/repo-private-key.pem --signedby "Bella Wagner <belladev109@proton.me>"
 xbps-rindex -r ~/cosmic-repo/repo/x86_64/
@@ -11,7 +10,6 @@ if [ "$1" != "glibc" ]; then
 	cp -n ~/void-packages/hostdir/binpkgs/cosmic*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
 	cp -n ~/void-packages/hostdir/binpkgs/pop-launcher*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
 	cp -n ~/void-packages/hostdir/binpkgs/catppuccin-sddm*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
-	cp -n ~/void-packages/hostdir/binpkgs/cosmic-iso-flatpaks*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
 	XBPS_TARGET_ARCH=x86_64-musl xbps-rindex -a ~/cosmic-repo/repo/x86_64-musl/*.xbps
 	xbps-rindex -S ~/cosmic-repo/repo/x86_64-musl/*.xbps --privkey ~/repo-private-key.pem --signedby "Bella Wagner <belladev109@proton.me>"
 	XBPS_TARGET_ARCH=x86_64-musl xbps-rindex -r ~/cosmic-repo/repo/x86_64-musl/
