a955e9820a3e7464a6061be640cc7876f5078165
MtFBella109 <belladev109@protonmail.com>
2025-10-01 18:05:10 +0200

    Added localectl

--- update-repo.sh
+++ update-repo.sh
@@ -4,6 +4,7 @@ cp -n ~/void-packages/hostdir/binpkgs/pop-*x86_64.xbps ~/cosmic-repo/repo/x86_64
 cp -n ~/void-packages/hostdir/binpkgs/setupIso*x86_64.xbps ~/cosmic-repo/repo/x86_64/
 cp -n ~/void-packages/hostdir/binpkgs/adw-gtk3*x86_64.xbps ~/cosmic-repo/repo/x86_64/
 cp -n ~/void-packages/hostdir/binpkgs/locale1*x86_64.xbps ~/cosmic-repo/repo/x86_64/
+cp -n ~/void-packages/hostdir/binpkgs/localectl*x86_64.xbps ~/cosmic-repo/repo/x86_64/
 xbps-rindex -a ~/cosmic-repo/repo/x86_64/*.xbps
 xbps-rindex -S ~/cosmic-repo/repo/x86_64/*.xbps --privkey ~/repo-private-key.pem --signedby "Bella Wagner <belladev109@proton.me>"
 xbps-rindex -r ~/cosmic-repo/repo/x86_64/
@@ -14,6 +15,7 @@ if [ "$1" != "glibc" ]; then
 	cp -n ~/void-packages/hostdir/binpkgs/setupIso*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
         cp -n ~/void-packages/hostdir/binpkgs/adw-gtk3*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
 	cp -n ~/void-packages/hostdir/binpkgs/locale1*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
+        cp -n ~/void-packages/hostdir/binpkgs/localectl*musl.xbps ~/cosmic-repo/repo/x86_64-musl/
 	XBPS_TARGET_ARCH=x86_64-musl xbps-rindex -a ~/cosmic-repo/repo/x86_64-musl/*.xbps
 	xbps-rindex -S ~/cosmic-repo/repo/x86_64-musl/*.xbps --privkey ~/repo-private-key.pem --signedby "Bella Wagner <belladev109@proton.me>"
 	XBPS_TARGET_ARCH=x86_64-musl xbps-rindex -r ~/cosmic-repo/repo/x86_64-musl/
